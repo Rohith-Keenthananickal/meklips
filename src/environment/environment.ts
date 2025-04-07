db91d9bf77a038c1ba550ea87afd126d29c11e4d
Rohith K Binu <rohith.keenthananickal@gmail.com>
2025-04-07 19:53:01 +0530

    build updated

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    // url : 'https://api.meklips.com/'
+    url : 'https://api.meklips.com/'
     // url : 'https://api.meklipsdev.shop'
-    url : 'http://localhost:8000/'
+    // url : 'http://localhost:8000/'
 }
